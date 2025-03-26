a47efa78250fa342a220906ee2cfc8e3cb9383e4
Jhamiya Jones <jhamiyaj@gmail.com>
2025-03-26 01:29:35 +0000

    added js practice

---
+++ script.js
@@ -0,0 +1,43 @@
+const num1 = 10;
+const num2 = 5;
+// const op = "product";
+// For basic js calculator practice
+
+
+// if (op == "sum"){
+//     let sum = num1 + num2;
+//     console.log(sum);
+// } else if (op == "sub"){
+//     let sub = num1 - num2;
+//     console.log(sub)
+// } else if (op == "quotient"){
+//     let quotient = num1 / num2;
+//     console.log(quotient)
+// } else if{
+//     let product = num1 * num2;
+//     console.log(product)
+// } else{
+//     console.log("something else")
+// }
+
+if(num1 > num2){
+    console.log("I am number one!");
+} else if(num1 < num2){
+    console.log("Best for last!");
+} else if(num1 >= num2){
+    console.log("Close Tie");
+} else if(num1 <= num2){
+    console.log("Almost is never enough.")
+} else{
+    console.log("NADA YADA")
+}
+
+
+
+// if(num1 == num2){
+//     console.log("same");
+// } else if(num1 != num2){
+//     console.log("not the same");
+// } else{
+//     console.log("something else");
+// }
